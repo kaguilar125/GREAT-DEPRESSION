ad8004050794b16cc4c9c89a9c49aa86b75945f3
Kevin <125kaguilarmartinez@frhsd.com>
2024-03-05 08:35:25 -0500

    push 4

---
+++ script.js
@@ -0,0 +1,48 @@
+function one() {
+    document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  }
+  function two() {
+    document.getElementById('img1').scr="img/Boeing_737-222,_Pan_American_World_Airways_-_Pan_Am_AN0472922.jpg" '';
+  }
+
+  function one1() {
+    document.getElementById('output').innerHTML = 'The Pan American World Airways, the largest airline in the United States began operations in October 1927. The airline experienced financial setbacks in the 1960s and 1970s, but it attempted to rebound by purchasing National Airlines in 1980, which allowed it to secure a vast network of routes throughout the eastern seaboard and locations west of the United States.';
+  }
+  function two1() {
+    document.getElementById('img1').scr="img/" '';
+  }
+
+  function one2() {
+    document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  }
+  function two2() {
+    document.getElementById('img1').scr="img/" '';
+  }
+
+  function one3() {
+    document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  }
+  function two3() {
+    document.getElementById('img1').scr="img/" '';
+  }
+
+  function one4() {
+    document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  }
+  function two4() {
+    document.getElementById('img1').scr="img/" '';
+  }
+
+  function one5() {
+    document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  }
+  function two5() {
+    document.getElementById('img1').scr="img/" '';
+  }
+
+  function one6() {
+    document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  }
+  function two6() {
+    document.getElementById('img1').scr="img/" '';
+  }
